cf23996e333cc238c6a999729b44a26c60adb258
PadBro <padmaster2012@googlemail.com>
2025-09-24 09:56:50 +0200

    feat(tickets): make panel message textarea

--- pages/ticket/panel/edit/[id].vue
+++ pages/ticket/panel/edit/[id].vue
@@ -20,7 +20,7 @@ const textChannels = ref(await loadTextChannels());
 const formSchema = toTypedSchema(
   zod.object({
     title: zod.string().min(1).max(100),
-    message: zod.string().min(1).max(100),
+    message: zod.string().min(1).max(1000),
     embed_color: zod.string().min(7).max(7),
     channel_id: zod.string(),
   }),
@@ -112,7 +112,7 @@ useHead({
       </div>
       <form class="grid grid-cols-1 gap-4" @submit.prevent="save">
         <FieldInput name="title" label="Title" />
-        <FieldInput name="message" label="Message" />
+        <FieldTextArea name="message" label="Message" />
         <FieldInput name="embed_color" type="color" label="Color" />
         <FieldSelect :items="textChannels" name="channel_id" label="Channel" />
 
