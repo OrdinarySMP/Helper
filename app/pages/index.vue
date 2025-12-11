46513dd8cdc6f4304d3f101b2453412b75b81386
PadBro <padmaster2012@googlemail.com>
2025-12-11 12:11:53 +0100

    feat: update dashboard

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,25 +1,5 @@
 <script lang="ts" setup>
-import { ref } from "vue";
-
 const user = useCurrentUser();
-const client = useApiClient();
-const toast = useSimpleToast();
-
-const botToken = ref<string>();
-const createTokenDialog = ref(false);
-
-const getBotToken = async () => {
-  const response = await client<{ token: string }>("/botToken");
-
-  if (response) {
-    botToken.value = response?.token;
-  } else {
-    toast.error("Failed to retrive bot token");
-  }
-
-  createTokenDialog.value = false;
-};
-
 useHead({
   title: "Dashboard",
 });
@@ -32,40 +12,12 @@ useHead({
     </template>
 
     <template #body>
-      <p>Hello {{ user?.nickname }}</p>
-      <UModal
-        v-model:open="createTokenDialog"
-        title="Create new bot token"
-        description="Creating a new token will invalidate all prevoius tokens!"
-      >
-        <UButton
-          v-if="hasPermissionTo('owner')"
-          label="Get bot token"
-          variant="subtle"
-          size="md"
-        />
-        <template #body>
-          <div class="flex justify-end gap-2">
-            <UButton
-              label="Cancel"
-              color="neutral"
-              variant="subtle"
-              size="md"
-              @click="createTokenDialog = false"
-            />
-            <UButton
-              label="Generate"
-              color="warning"
-              size="md"
-              @click="getBotToken"
-            />
-          </div>
-        </template>
-      </UModal>
-
-      <p v-if="botToken">
-        {{ botToken }}
-      </p>
+      <p class="text-2xl">Hello {{ user?.nickname }}</p>
+      <div class="columns-1 md:columns-2 gap-4">
+        <CardApplication class="mb-4" />
+        <CardTicket class="mb-4" />
+        <CardBotToken class="mb-4" />
+      </div>
     </template>
   </UDashboardPanel>
 </template>
